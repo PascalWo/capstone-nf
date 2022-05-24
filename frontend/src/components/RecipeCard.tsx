6a7f459e80c5bf5dbb0894dd351b0460090b1809
PascalWo <pwolff.de@gmail.com>
2022-05-24 16:19:18 +0200

    added styling for RecipeCard and Overview

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,5 +1,7 @@
 import {Recipe} from "../model/Recipe";
 import "./RecipeCard.css";
+import recipeDefaultImg from "./recipeimage.png"
+import {SyntheticEvent} from "react";
 
 type RecipeCardProps = {
     recipe: Recipe;
@@ -9,9 +11,16 @@ export default function RecipeCard({recipe}: RecipeCardProps){
 
     return(
         <div className={"recipe-card"}>
-            <p>{recipe.id}</p>
-            <p>{recipe.title}</p>
-            <p>{recipe.image}</p>
+            <div id={"recipe-title"}>{recipe.title}</div>
+            {recipe.image
+            ? <img id={"recipe-image"}
+                   src={recipe.image}
+                   onError={(event: SyntheticEvent<HTMLImageElement>) => event.currentTarget.src = recipeDefaultImg}
+                   alt={"Recipe"}/>
+            : <img id={"recipe-image"}
+                   src={recipeDefaultImg}
+                   alt={"Recipe Default"} />}
+            <div id={"recipe-description"}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
         </div>
     )
 
