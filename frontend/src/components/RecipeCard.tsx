26540091005852baaced9c0d7b6845eb4048cf05
PascalWo <pwolff.de@gmail.com>
2022-05-25 20:54:50 +0200

    trouble with description styling

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -9,6 +9,7 @@ type RecipeCardProps = {
 
 export default function RecipeCard({recipe}: RecipeCardProps){
 
+
     return(
         <div className={"recipe-card"}>
             <div id={"recipe-title"}>{recipe.title}</div>
@@ -20,7 +21,8 @@ export default function RecipeCard({recipe}: RecipeCardProps){
             : <img id={"recipe-image"}
                    src={recipeDefaultImg}
                    alt={"Recipe Default"} />}
-            <div id={"recipe-description"}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+            <div id={"recipe-description"}>{recipe.summary?.substring(0,100)}</div>
+
         </div>
     )
 
