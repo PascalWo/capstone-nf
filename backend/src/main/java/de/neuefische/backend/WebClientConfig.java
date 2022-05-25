fecd332cefcd2deccd4b11b6646ce06470ffac66
PascalWo <pwolff.de@gmail.com>
2022-05-25 14:19:41 +0200

    changed IntegrationsTest to WireMock

--- backend/src/main/java/de/neuefische/backend/WebClientConfig.java
+++ backend/src/main/java/de/neuefische/backend/WebClientConfig.java
@@ -1,12 +1,16 @@
 package de.neuefische.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
+    @Value("${spoona.api.baseurl}")
+    private String baseUrl;
+
     public WebClient getWebClient() {
-        return WebClient.create("https://api.spoonacular.com/recipes");
+        return WebClient.create(baseUrl);
     }
 }
--- backend/src/main/java/de/neuefische/backend/api/SpoonacularApiService.java
+++ backend/src/main/java/de/neuefische/backend/api/SpoonacularApiService.java
@@ -1,6 +1,5 @@
 package de.neuefische.backend.api;
 
-import de.neuefische.backend.WebClientConfig;
 import de.neuefische.backend.errorhandling.ApiNotRespondException;
 import de.neuefische.backend.model.Recipe;
 import de.neuefische.backend.model.RecipeInfo;
@@ -9,27 +8,30 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 @Service
 public class SpoonacularApiService {
 
-    private final WebClientConfig webClientConfig;
+    private final WebClient webClient;
 
-    public SpoonacularApiService(WebClientConfig webClientConfig) {
-        this.webClientConfig = webClientConfig;
+    public SpoonacularApiService(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     @Value("${spoonacular.lapamealfactory.api.key}")
     private String API_KEY;
 
+    @Value("${spoona.api.baseurl}")
+    private String baseUrl;
+
     public List<Recipe> retrieveRecipes(String search) {
 
-            ResponseEntity<RecipeInfo> responseEntity = webClientConfig
-                    .getWebClient()
+            ResponseEntity<RecipeInfo> responseEntity = webClient
                     .get()
-                    .uri("/complexSearch?query=" + search)
+                    .uri(baseUrl + "/complexSearch?query=" + search)
                     .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                     .header("x-api-key", API_KEY)
                     .retrieve()
@@ -42,7 +44,7 @@ public class SpoonacularApiService {
             RecipeInfo recipeInfo = responseEntity.getBody();
 
             if (recipeInfo == null){
-                throw new ApiNotRespondException("API not available");
+                throw new ApiNotRespondException("Response is Null");
             }
 
             return recipeInfo.getResults();
