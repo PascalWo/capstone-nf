6bd1c384a623445d966da1639e4ab5e813481272
PascalWo <pwolff.de@gmail.com>
2022-06-01 11:52:33 +0200

    refactored detailspage general info + ingredients

--- frontend/src/components/ShowIngredients.tsx
+++ frontend/src/components/ShowIngredients.tsx
@@ -7,11 +7,15 @@ type ShowIngredientsProps = {
 
 export default function ShowIngredients({recipe}: ShowIngredientsProps){
     return  (
-        <div className={"ingredients"}>
-            <div>{recipe.extendedIngredients && recipe.extendedIngredients.map(ingredients => (
-                <div key={ingredients.name}>Zutat: {ingredients.name}</div>))}</div>
-            <div>{recipe.extendedIngredients && recipe.extendedIngredients.map(ingredients => (
-                <div key={ingredients.amount}>Anzahl:{ingredients.amount}</div>))}</div>
+        <div className={"show-ingredients"}>
+            <div id={"ingredient"}>Zutat: {recipe.extendedIngredients && recipe.extendedIngredients
+                .map(ingredients => (
+                    <div id={"ingredient-name"} key={ingredients.name}>{ingredients.name}</div>))}
+            </div>
+            <div id={"amount"}>Menge:{recipe.extendedIngredients && recipe.extendedIngredients
+                .map(ingredients => (
+                    <div id={"ingredient-amount"} key={ingredients.amount}>{ingredients.amount}</div>))}
+            </div>
         </div>
     )
 }
