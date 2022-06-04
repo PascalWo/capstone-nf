c54127177dbde55fba898c6d4d15c6277af051b1
PascalWo <pwolff.de@gmail.com>
2022-06-04 16:15:23 +0200

    added backend logic for create new recipe equipment attribute

--- backend/src/main/java/de/neuefische/backend/service/SpoonacularService.java
+++ backend/src/main/java/de/neuefische/backend/service/SpoonacularService.java
@@ -1,10 +1,12 @@
 package de.neuefische.backend.service;
 
 import de.neuefische.backend.api.SpoonacularApiService;
+import de.neuefische.backend.model.Equipment;
 import de.neuefische.backend.model.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -31,6 +33,28 @@ public class SpoonacularService {
                                 .forEach(equipment -> equipment
                                         .setImage("https://spoonacular.com/cdn/equipment_100x100/" + equipment
                                                 .getImage()))));
+
+        ArrayList<Equipment> equipmentList = new ArrayList<>();
+        recipe.getAnalyzedInstructions()
+                .forEach(instruction -> instruction
+                        .getSteps()
+                        .forEach(instructionStep -> instructionStep
+                                .getEquipment()
+                                .forEach(equipment -> equipmentList.add(equipment))));
+
+        System.out.println(equipmentList);
+
+         ArrayList<Equipment> newList = new ArrayList<>();
+        for (Equipment equipment: equipmentList) {
+            if (!newList.contains(equipment)){
+                newList.add(equipment);
+            }
+        }
+        System.out.println(newList);
+        recipe.setEquipment(newList);
+
+        System.out.println(recipe);
+
         return recipe;
     }
 }
