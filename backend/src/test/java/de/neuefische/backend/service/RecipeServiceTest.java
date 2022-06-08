f0f6a5a6839a6a11571534d4840e096a9ffa18c1
PascalWo <pwolff.de@gmail.com>
2022-06-08 19:18:58 +0200

    added backend logic for edit Recipe

--- backend/src/test/java/de/neuefische/backend/service/RecipeServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/RecipeServiceTest.java
@@ -1,7 +1,7 @@
 package de.neuefische.backend.service;
 
 import de.neuefische.backend.dto.CreateRecipeDto;
-import de.neuefische.backend.model.Recipe;
+import de.neuefische.backend.model.*;
 import de.neuefische.backend.repository.RecipeRepo;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +17,44 @@ class RecipeServiceTest {
 
     private final RecipeRepo recipeRepo = mock(RecipeRepo.class);
     private final RecipeService recipeService = new RecipeService(recipeRepo);
+
+    Ingredient ingredient1 = Ingredient.builder()
+            .name("Pasta")
+            .amount(500)
+            .unit("pound")
+            .build();
+    Ingredient ingredient2 = Ingredient.builder()
+            .name("Tomato")
+            .amount(3)
+            .unit("Stk.")
+            .build();
+
+    Equipment equipmentWithUri1 = Equipment.builder()
+            .id(1)
+            .name("oven")
+            .image("https://spoonacular.com/cdn/equipment_100x100/oven-image")
+            .build();
+    Equipment equipmentWithUri2 = Equipment.builder()
+            .id(2)
+            .name("bowl")
+            .image("https://spoonacular.com/cdn/equipment_100x100/bowl-image")
+            .build();
+
+    InstructionStep instructionWithUriStep1 = InstructionStep.builder()
+            .number(1)
+            .step("clean Ingredients")
+            .equipment(List.of(equipmentWithUri1))
+            .build();
+    InstructionStep instructionWithUriStep2 = InstructionStep.builder()
+            .number(2)
+            .step("cook Ingredients")
+            .equipment(List.of(equipmentWithUri2))
+            .build();
+
+    Instruction instructionWithUri = Instruction.builder()
+            .name("Main dish")
+            .steps(List.of(instructionWithUriStep1,instructionWithUriStep2))
+            .build();
 
     @Test
     void getRecipes() {
@@ -130,5 +168,110 @@ class RecipeServiceTest {
         //WHEN //THEN
         assertThrows(NoSuchElementException.class, () -> recipeService.getRecipeDetails("1"));
         verify(recipeRepo).findById("1");
+    }
+
+    @Test
+    void updateRecipeByID_whenIdExists_shouldReturnRecipe() {
+        //GIVEN
+        String updateRecipeId = "123";
+
+        CreateRecipeDto updatedRecipe =CreateRecipeDto.builder()
+                .title("Pasta Special")
+                .image("Image")
+                .vegetarian(true)
+                .vegan(true)
+                .glutenFree(true)
+                .readyInMinutes(30)
+                .servings(2)
+                .summary("Best Recipe ever")
+                .extendedIngredients(List.of(ingredient1, ingredient2))
+                .analyzedInstructions(List.of(instructionWithUri))
+                .equipment(List.of(equipmentWithUri1, equipmentWithUri2))
+                .build();
+
+        Recipe saveRecipe =Recipe.builder()
+                .id("123")
+                .title("Pasta Special")
+                .image("Image")
+                .vegetarian(true)
+                .vegan(true)
+                .glutenFree(true)
+                .readyInMinutes(30)
+                .servings(2)
+                .summary("Best Recipe ever")
+                .extendedIngredients(List.of(ingredient1, ingredient2))
+                .analyzedInstructions(List.of(instructionWithUri))
+                .equipment(List.of(equipmentWithUri1, equipmentWithUri2))
+                .build();
+
+        when(recipeRepo.existsById(updateRecipeId)).thenReturn(true);
+        when(recipeRepo.save(saveRecipe)).thenReturn(saveRecipe);
+
+        //WHEN
+
+        Recipe actual = recipeService.updateRecipeByID(updateRecipeId, updatedRecipe);
+        //THEN
+        verify(recipeRepo).save(saveRecipe);
+        assertEquals(saveRecipe, actual);
+    }
+
+    @Test
+    void updateRecipeByID_whenIdNotExists_shouldReturnException(){
+        //GIVEN
+        String updateRecipeId = "123";
+
+        CreateRecipeDto updatedRecipe =CreateRecipeDto.builder()
+                .title("Pasta Special")
+                .image("Image")
+                .vegetarian(true)
+                .vegan(true)
+                .glutenFree(true)
+                .readyInMinutes(30)
+                .servings(2)
+                .summary("Best Recipe ever")
+                .extendedIngredients(List.of(ingredient1, ingredient2))
+                .analyzedInstructions(List.of(instructionWithUri))
+                .equipment(List.of(equipmentWithUri1, equipmentWithUri2))
+                .build();
+
+        //WHEN //THEN
+        assertThrows(NoSuchElementException.class, () -> recipeService.updateRecipeByID(updateRecipeId, updatedRecipe));
+    }
+
+    @Test
+    void createRecipeFromDto() {
+        //GIVEN
+        CreateRecipeDto recipeDto = CreateRecipeDto.builder()
+                .title("Pasta Special")
+                .image("Image")
+                .vegetarian(true)
+                .vegan(true)
+                .glutenFree(true)
+                .readyInMinutes(30)
+                .servings(2)
+                .summary("Best Recipe ever")
+                .extendedIngredients(List.of(ingredient1, ingredient2))
+                .analyzedInstructions(List.of(instructionWithUri))
+                .equipment(List.of(equipmentWithUri1, equipmentWithUri2))
+                .build();
+
+        //WHEN
+        Recipe actual = recipeService.createRecipeFromDto(recipeDto);
+        //THEN
+        Recipe expected = Recipe.builder()
+                .title("Pasta Special")
+                .image("Image")
+                .vegetarian(true)
+                .vegan(true)
+                .glutenFree(true)
+                .readyInMinutes(30)
+                .servings(2)
+                .summary("Best Recipe ever")
+                .extendedIngredients(List.of(ingredient1, ingredient2))
+                .analyzedInstructions(List.of(instructionWithUri))
+                .equipment(List.of(equipmentWithUri1, equipmentWithUri2))
+                .build();
+
+        assertEquals(expected, actual);
     }
 }
