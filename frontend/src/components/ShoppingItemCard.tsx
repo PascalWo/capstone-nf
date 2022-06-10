9e51c37e91530755e5346348aed8d6838f79596d
PascalWo <pwolff.de@gmail.com>
2022-06-10 11:44:33 +0200

    fixed some formatting

--- frontend/src/components/ShoppingItemCard.tsx
+++ frontend/src/components/ShoppingItemCard.tsx
@@ -9,7 +9,6 @@ export default function ShoppingItemCard({shoppingItem}:ShoppingItemCardProps){
     return (
         <div className={"shopping-item-card"}>
             <div id={"item-container"}>
-                {/*<div id={"bulletpoint"}></div>*/}
                 <div id={"name-bulletpoint"}>
                     <div id={"bulletpoint"} ></div>
                     <div id={shoppingItem.done? "done-item": "undone-item"}>{shoppingItem.name}</div>
