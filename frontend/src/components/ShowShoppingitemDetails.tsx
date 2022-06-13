9b22804adef7594423c38607ab62a2149e3abfc6
PascalWo <pwolff.de@gmail.com>
2022-06-13 11:37:16 +0200

    added back buttons for shopping items

--- frontend/src/components/ShowShoppingitemDetails.tsx
+++ frontend/src/components/ShowShoppingitemDetails.tsx
@@ -1,4 +1,6 @@
 import {ShoppingItem} from "../model/ShoppingItem";
+import * as AiIcons from "react-icons/ai";
+import {useNavigate} from "react-router-dom";
 
 type ShowShoppingItemDetailsProps = {
     item: ShoppingItem
@@ -6,11 +8,20 @@ type ShowShoppingItemDetailsProps = {
 }
 
 export default function ShowShoppingItemDetails({item, toggleEditing} : ShowShoppingItemDetailsProps){
+    const navigate =useNavigate()
+
     return(
         <div className={"show-shopping-item-details"}>
             <p className={item.done? "done-item": "undone-item"}>
                 {"Item: " + item.name + "Amount: " + item.amount + "Unit: " + item.unit}</p>
-            <button onClick={toggleEditing}>Edit item</button>
+           <div id={"shopping-details-button"}>
+            <AiIcons.AiFillEdit
+                id={"details-button-symbol"}
+                onClick={toggleEditing}/>
+            <AiIcons.AiFillLeftCircle
+                id={"details-button-symbol"}
+                onClick={() => navigate(-1) }/>
+           </div>
         </div>
     )
 }
